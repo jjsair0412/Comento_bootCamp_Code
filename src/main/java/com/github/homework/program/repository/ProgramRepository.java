6c9f0ea87e25ee70f9d0d1cd8840f2b4843dd4db
codementor-sw <melpis0215@gmail.com>
2021-07-12 19:03:06 +0900

    최초 커밋

---
+++ src/main/java/com/github/homework/program/repository/ProgramRepository.java
@@ -0,0 +1,7 @@
+package com.github.homework.program.repository;
+
+import com.github.homework.program.domain.Program;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProgramRepository extends JpaRepository<Program, Long>, ProgramCustomRepository {
+}
